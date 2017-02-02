bd2586e693f1d7ec39f204a295a3d44f155ccd8b
Richard Strauss <rstrauss91@gmail.com>
2017-02-02 16:46:58 -0500

    Create validations and errors messages for photo uploads

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,5 @@
 class Picture < ApplicationRecord
+validates :artist, presence: {message: "'et that name right'"}
+validates :title, length: { in: 3..20, message: "'et that len'th ri'ht" }
+validates :url, presence: true, uniqueness: true, format: { with: /\A[h][t][t][p][:][\/][\/]/ , message: "start that url right"}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170127192738) do
+ActiveRecord::Schema.define(version: 20170202204214) do
 
   create_table "pictures", force: :cascade do |t|
     t.string   "artist"
